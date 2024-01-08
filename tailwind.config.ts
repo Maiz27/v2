6334c030a90e26f044a34b1a5535782836d35a77
Maged Faiz <magedfaiz98@gmail.com>
2024-01-08 16:11:01 +0200

    chore: removed starter code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,35 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: '#00adb5',
+        'primary-content': '#b5fcff',
+        'primary-dark': '#007c82',
+        'primary-light': '#00dee8',
+
+        secondary: '#00b535',
+        'secondary-content': '#b5ffcb',
+        'secondary-dark': '#008226',
+        'secondary-light': '#00e844',
+
+        background: '#ebf4f4',
+        foreground: '#fafcfc',
+        border: '#d6e7e8',
+
+        copy: '#1c3031',
+        'copy-light': '#4a8082',
+        'copy-lighter': '#6da9ac',
+
+        success: '#00b500',
+        warning: '#b5b500',
+        error: '#b50000',
+
+        'success-content': '#b5ffb5',
+        'warning-content': '#000000',
+        'error-content': '#ffb5b5',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
