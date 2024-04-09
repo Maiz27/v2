f660816b088cc68c07e023d2a8dedaf1d2f89af9
Maged Faiz <magedfaiz98@gmail.com>
2024-04-09 18:58:10 +0200

    fix: wrapped Project Filter with Suspense

--- src/components/projects/ProjectsFilter.tsx
+++ src/components/projects/ProjectsFilter.tsx
@@ -4,19 +4,30 @@ import CTA from '@/components/CTA/CTA';
 import IconCTA from '@/components/CTA/IconCTA';
 import { PROJECT_STATUS } from '@/lib/Constants';
 import useProjectFilters from '@/lib/hooks/useProjectFilters';
+import { Suspense } from 'react';
 import { HiOutlineMinusCircle } from 'react-icons/hi2';
 
 const ProjectsFilter = ({ projectsTotal }: { projectsTotal: number }) => {
+  return (
+    <Suspense>
+      <Filter projectsTotal={projectsTotal} />
+    </Suspense>
+  );
+};
+
+export default ProjectsFilter;
+
+const Filter = ({ projectsTotal }: { projectsTotal: number }) => {
   const { state, handleChange, handleTechSelection, resetFilters } =
     useProjectFilters();
 
   return (
     <div>
       <div className='flex flex-col-reverse lg:flex-row justify-between mt-5 gap-2'>
-        <div className='text-xl text-center lg:text-left my-auto'>
+        <h2 className='text-xl text-center lg:text-left my-auto'>
           <span className='text-primary opacity-100 pr-2'>{projectsTotal}</span>
           Projects
-        </div>
+        </h2>
 
         <div className='flex flex-col lg:flex-row lg:items-center gap-2'>
           <select
@@ -66,8 +77,6 @@ const ProjectsFilter = ({ projectsTotal }: { projectsTotal: number }) => {
     </div>
   );
 };
-
-export default ProjectsFilter;
 
 const FilterInfo = ({
   label,
